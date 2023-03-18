e497c8fc6d1a58d1078208b9b77104c687bac8f0
AkashJagtap0003 <127941729+AkashJagtap0003@users.noreply.github.com>
2023-03-18 17:21:03 +0530

    Update Arrays2.java

--- Arrays2.java
+++ Arrays2.java
@@ -3,16 +3,16 @@ class Arrays2{
 	public static void main(String[] args){
 		
 		int arr[]=new int[]{5,4,9,2,7};
-		int max=arr[0];
+		int min=arr[0];
 		for(int i=1;i<5;i++){
-				if(arr[i]>max){
-					max=max;
+				if(arr[i]>min){
+					min=min;
 				}
 				else{
-					max=arr[i];
+					min=arr[i];
 				}
 			
 		}
-		System.out.println(max);
+		System.out.println(min);
 	}
 }
