efc56adb6ce6553537172b232a1a55b5a1d03a61
AkashJagtap0003 <127941729+AkashJagtap0003@users.noreply.github.com>
2023-03-18 17:24:07 +0530

    Add files via upload

---
+++ Arrays3.java
@@ -0,0 +1,21 @@
+class Arrays3{
+	//Search
+	public static void main(String[] args){
+		
+		int arr[]=new int[]{5,4,9,2,7};
+		int search=3;
+		
+		String max= new String();
+		for(int i=0;i<5;i++){
+				if(arr[i]==search){
+					max="present";
+					break;
+				}
+				else{
+					max="absent";
+				}
+			
+		}
+		System.out.println(max);
+	}
+}
